d972d5d8317b9ee9571fb69a37647fbaf619223a
gowdaman-dev <damangowdaman@gmail.com>
2024-10-29 21:04:59 +0530

    new

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -6,7 +6,7 @@ import HeroImg from './HeroImg'
 
 function Hero() {
     return (
-        <div className="w-full min-h-[515px] overflow-hidden flex flex-nowrap md:flex-row flex-col justify-between items-start content-space-between py-20">
+        <div className="w-full min-h-[515px] overflow-hidden flex flex-nowrap md:flex-row flex-col justify-between items-start content-space-between md:py-20 py-10">
             <div className="w-full box-border min-h-fit  flex flex-col flex-nowrap justify-start items-start gap-6">
                 <div className="">
                     <span className="text-6xl font-black text-[#000000ff]">
