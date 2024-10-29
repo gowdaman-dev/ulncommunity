60c72d1d4ebeb715d1edecc68a1e1bb8b579d530
gowdaman-dev <damangowdaman@gmail.com>
2024-10-29 21:09:26 +0530

    new

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -9,7 +9,7 @@ function Hero() {
         <div className="w-full min-h-[515px] overflow-hidden flex flex-nowrap md:flex-row flex-col justify-between items-start content-space-between md:py-20 py-10">
             <div className="w-full box-border min-h-fit  flex flex-col flex-nowrap justify-start items-start gap-6">
                 <div className="">
-                    <span className="text-6xl font-black text-[#000000ff]">
+                    <span className="md:text-6xl text-4xl font-black text-[#000000ff]">
                     Crafting Digital Solutions for a New Era of Innovation
                     </span>
                 </div>
